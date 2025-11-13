36ea57bdc4edacc992e2d8dcff5e294afbc5d2ae
van-dt <dangvan31102001@gmail.com>
2025-11-13 11:40:43 +0700

    chore: update link open behavior

--- src/services/subscription.service.ts
+++ src/services/subscription.service.ts
@@ -3,10 +3,15 @@ import apiClient from "../lib/axios";
 export const subscriptionService = {
   upgradeToPro: async () => {
     try {
-      const { data } = await apiClient.post("/subscriptions", {
-        plan: "pro",
-      });
-      window.open(data.paymentUrl, "_blank");
+      const paymentWindow = window.open("", "_blank");
+      const { data } = await apiClient.post("/subscriptions", { plan: "pro" });
+
+      if (paymentWindow) {
+        paymentWindow.opener = null;
+        paymentWindow.location.href = data.paymentUrl;
+      } else {
+        window.location.href = data.paymentUrl;
+      }
     } catch (error) {
       console.error("Failed to handle payment", error);
     }

041ada731f2bd5df62d722808a45a6d6908380bb
diptszyx <ndtho2003@@gmail.com>
2026-02-23 19:04:39 +0700

    add mwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,23 @@
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
+import {
+  createDefaultAuthorizationCache,
+  createDefaultChainSelector,
+  createDefaultWalletNotFoundHandler,
+  registerMwa,
+} from '@solana-mobile/wallet-standard-mobile';
+
+registerMwa({
+  appIdentity: {
+    name: 'Predit Market',
+    uri: 'https://predit.market',
+    icon: 'logo-MarkWhite.svg',
+  },
+  authorizationCache: createDefaultAuthorizationCache(),
+  chains: ['solana:devnet', 'solana:mainnet'],
+  chainSelector: createDefaultChainSelector(),
+  onWalletNotFound: createDefaultWalletNotFoundHandler(),
+});
 
 createRoot(document.getElementById('root')!).render(<App />);

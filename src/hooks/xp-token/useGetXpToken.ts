ada182a0368639691cb1a16d35c1150d83a88cf9
van-dt <dangvan31102001@gmail.com>
2026-03-24 10:19:11 +0700

    chore: fix useEffect issue

--- src/hooks/xp-token/useGetXpToken.ts
+++ src/hooks/xp-token/useGetXpToken.ts
@@ -4,18 +4,19 @@ import { getXpTokenBalance } from "../../services/claim-token.service";
 
 const useGetXpToken = (user: User | null) => {
   const [xpToken, setXpToken] = useState(0);
-  if (!user) return { xpToken };
-
-  const fetchPreditXpToken = async () => {
-    try {
-      const xpToken = await getXpTokenBalance();
-      setXpToken(xpToken.balance);
-    } catch (error) {
-      console.log("Failed to fetch xp token", error);
-    }
-  };
 
   useEffect(() => {
+    if (!user) return;
+
+    const fetchPreditXpToken = async () => {
+      try {
+        const xpToken = await getXpTokenBalance();
+        setXpToken(xpToken.balance);
+      } catch (error) {
+        console.log("Failed to fetch xp token", error);
+      }
+    };
+
     fetchPreditXpToken();
   }, [user]);
 
